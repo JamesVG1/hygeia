b61c3d637b2effb14b8fd8c0ce761f5ec5dba29d
benhbaik <benhbaik@gmail.com>
2016-09-07 14:42:20 -0600

    started provider model

---
+++ app/models/providerModel.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var providerSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }
+});
+
+module.exports = mongoose.model('Provider', providerSchema);
